7ac005a4e24ac6e4e89d3ca20fae6c3a2401d068
Taylor Huang <34359329+TaylorHuang2017@users.noreply.github.com>
2023-07-01 14:15:30 +0800

    upload files

---
+++ precache-manifest.52dfc5f6bb2259ebf62f10f6f3f2cc2d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd32cfb2b5d08bfb813b",
+    "url": "./d032240921e3b8f45d06.module.wasm"
+  },
+  {
+    "revision": "724baf04ad405e27c28c619ca9073c01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64d0ec7041608d73c989",
+    "url": "./static/css/2.c412fb8a.chunk.css"
+  },
+  {
+    "revision": "792bbc4dc6c52c348353",
+    "url": "./static/css/main.34bf5f7b.chunk.css"
+  },
+  {
+    "revision": "64d0ec7041608d73c989",
+    "url": "./static/js/2.33e4708c.chunk.js"
+  },
+  {
+    "revision": "fd32cfb2b5d08bfb813b",
+    "url": "./static/js/3.bc5a8cfa.chunk.js"
+  },
+  {
+    "revision": "792bbc4dc6c52c348353",
+    "url": "./static/js/main.b1f94d87.chunk.js"
+  },
+  {
+    "revision": "a53c3657d5b2dc6048af",
+    "url": "./static/js/runtime-main.8406c0a2.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "./static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "./static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "./static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "./static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "./static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "./static/media/icons-20.cef8cdbb.woff"
+  }
+]);
